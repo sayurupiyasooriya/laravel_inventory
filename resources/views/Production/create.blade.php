c6d04bcaf12ab92447edd0939193b2bc1ce8bf12
sayurupiyasooriya <snpiyasooriya@gmail.com>
2021-06-14 17:55:57 +0530

    production create

--- resources/views/Production/create.blade.php
+++ resources/views/Production/create.blade.php
@@ -1,182 +1,180 @@
 @extends('layouts.backend.app')
 @push('css')
-<link rel="stylesheet" href="{{ asset('asset/css/custom.css') }}">
+    {{-- <link rel="stylesheet" href="{{ asset('asset/css/custom.css') }}"> --}}
 @endpush
 @section('content')
 
-<!-- Content Wrapper. Contains page content -->
-<div class="content-wrapper">
-    <!-- Content Header (Page header) -->
-    <div class="content-header">
-        <div class="container-fluid">
-            <div class="row mb-2">
-                <div class="col-sm-6">
-                    <h1 class="m-0 text-dark">Productions</h1>
-                </div><!-- /.col -->
-            </div><!-- /.row -->
-        </div><!-- /.container-fluid -->
-    </div>
-
-    <div class="card">
-        <div class="card-header">
-            <h3 class="card-title">Insert Productions</h3>
-            <span align-left>
-                {{-- <a class="btn btn-action btn-primary float-right" title="Create" id="create">Create</a> --}}
-
-            </span>
+    <!-- Content Wrapper. Contains page content -->
+    <div class="content-wrapper">
+        <!-- Content Header (Page header) -->
+        <div class="content-header">
+            <div class="container-fluid">
+                <div class="row mb-2">
+                    <div class="col-sm-6">
+                        <h1 class="m-0 text-dark">Productions</h1>
+                    </div><!-- /.col -->
+                </div><!-- /.row -->
+            </div><!-- /.container-fluid -->
         </div>
-        <!-- /.card-header -->
-        <div class="container">
 
-            <form method="POST" action="">
-                {{-- select supplier --}}
-                {{ csrf_field() }}
-                <div class="form-inline">
-                    <div class="form-group ml-2">
-                        <label for="supplier_id">Select Supplier</label>
-                        <select name="supplier_id" id="supplier_id" class="form-control supplier_id">
-                            <option selected="selected" value="">Select a Supplier</option>
-                            @foreach ($buyers as $buyer)
-                            <option value="{{$buyer->id}}">{{$buyer->name}}</option>
-                            @endforeach
-                        </select>
-                    </div>
+        <div class="card">
+            <div class="card-header">
+                <h3 class="card-title">Insert Productions</h3>
+                <span align-left>
+                    {{-- <a class="btn btn-action btn-primary float-right" title="Create" id="create">Create</a> --}}
 
-                    {{-- select product --}}
-                    <div class="form-group ml-2">
-                        <label for="prd_cat">Select Production Category</label>
-                        <select name="prd_cat" id="prd_cat" class="form-control">
-                            @foreach ($productCat as $product)
-                            <option value="{{$product->id}}">{{$product->type}}</option>
-                            @endforeach
-                        </select>
-                    </div>
+                </span>
+            </div>
+            <!-- /.card-header -->
+            <div class="container">
 
-                    <div class="form-group ml-2">
-                        <label for="product-name">Enter Product Name</label>
-                        <input type="text" name="product-name" id="production-name">
-                    </div>
+                <form method="POST" action="">
+                    {{-- select supplier --}}
+                    {{ csrf_field() }}
+                    <div class="form-inline">
+                        <div class="form-group ml-2">
+                            <select name="supplier_id" id="supplier_id" class="form-control supplier_id">
+                                <option selected="selected" value="">Select a Supplier</option>
+                                @foreach ($buyers as $buyer)
+                                    <option value="{{ $buyer->id }}">{{ $buyer->name }}</option>
+                                @endforeach
+                            </select>
+                        </div>
 
-                    <div class="form-group ml-2 ">
-                        <input type="text" value="" id="add-field" name="product-name">
-                    </div>
-                </div>
+                        {{-- select product --}}
+                        <div class="form-group ml-2">
+                            <select name="prd_cat" id="prd_cat" class="form-control">
+                                <option selected="selected" value="">Select Product category</option>
+                                @foreach ($productCat as $product)
+                                    <option value="{{ $product->id }}">{{ $product->type }}</option>
+                                @endforeach
+                            </select>
+                        </div>
 
+                        <div class="form-group ml-2">
+                            <input class="form-control" type="text" name="product-name" id="production-name"
+                                placeholder="Enter Product Name">
+                        </div>
 
-                <div class="row">
-
-                    <div class="col-sm-8 float-right">
-                        <div class="append" id="append" name="append" style="height:224px; overflow:auto;">
-                            <ul>
-
-                            </ul>
+                        <div class="form-group ml-2 ">
+                            <input class="form-control" type="text" value="" id="add-field" name="product-name">
                         </div>
                     </div>
-                </div>
-                {{-- <a class="btn btn-action btn-warning float-right" title="Save" onclick="save_data()"  id="save">Save</a> --}}
-                <input type="submit" name="create" id="create">
-                <a class="btn btn-action btn-danger float-left" title="Delete" id="del">Delete</a>
-                <input type="submit" name="saveData" id="saveData" value="Save" class="btn btn-warning float-right">
 
-                {{-- <a class="btn btn-action btn-primary float-right" title="Create" id="create">Create</a> --}}
-            </form>
+
+                    <div class="row">
+
+                        <div class="col-sm-8 float-right">
+                            <div class="append" id="append" name="append" style="height:224px; overflow:auto;">
+                                <ul>
+
+                                </ul>
+                            </div>
+                        </div>
+                    </div>
+                    {{-- <a class="btn btn-action btn-warning float-right" title="Save" onclick="save_data()"  id="save">Save</a> --}}
+                    <div class="float-left">
+                        <input type="submit" name="saveData" id="saveData" value="Save" class="btn btn-warning">
+                        <a class="btn btn-action btn-danger" title="Delete" id="del">Delete</a>
+                    </div>
+                </form>
+
+
+            </div>
 
 
         </div>
 
-
-    </div>
-
-    <!-- /.card -->
+        <!-- /.card -->
     @endsection
 
     @push('js')
-    <script>
-        function addItems() {
-            var addField = $('#add-field');
-            var addfleidText = addField.val();
+        <script>
+            function addItems() {
+                var addField = $('#add-field');
+                var addfleidText = addField.val();
 
-            var appendableItem = ('<li><div class="item" txt="' + addfleidText + '" onclick="itemSelect(this)">' +
-                addfleidText + '</div></li>');
+                var appendableItem = ('<li><div class="item" txt="' + addfleidText + '" onclick="itemSelect(this)">' +
+                    addfleidText + '</div></li>');
 
-            $('.append > ul').append(appendableItem);
-            $('#add-field').val("");
-        }
+                $('.append > ul').append(appendableItem);
+                $('#add-field').val("");
+            }
 
-        function deleteField() {
-            $('.append>ul>li>.selected').remove();
-        }
+            function deleteField() {
+                $('.append>ul>li>.selected').remove();
+            }
 
-        function dleteAll() {
-            $('.append>ul>li').remove();
-        }
+            function dleteAll() {
+                $('.append>ul>li').remove();
+            }
 
-        function itemSelect() {
-            $(event.target).toggleClass('selected');
-        }
+            function itemSelect() {
+                $(event.target).toggleClass('selected');
+            }
 
-        function removeAll(){
-            $('.append > ul > li').remove();
-        }
-
-
+            function removeAll() {
+                $('.append > ul > li').remove();
+            }
 
 
-        $(document).ready(function () {
-            // $('#create').attr('onclick', 'addItems()');
-            $("#supplier_id").change(function () {
 
-            })
 
-            $('#create').click(function (e) {
-                e.preventDefault();
-                addItems();
+            $(document).ready(function() {
+                // $('#create').attr('onclick', 'addItems()');
+                $("#supplier_id").change(function() {
+
+                })
+
+                $('#create').click(function(e) {
+                    e.preventDefault();
+                    addItems();
+                });
+
+                $('#del').attr('onclick', 'deleteField()');
+
+                // save form data
+                $('#saveData').click(function(e) {
+                    e.preventDefault();
+
+                    var supplierId = $("#supplier_id").val();
+                    var prdCatId = $("#prd_cat").val();
+                    var prdName = $("#production-name").val();
+                    // serial numbers
+                    var seldata = [];
+                    $('.append').find(".item").each(function() {
+                        seldata.push($(this).attr("txt"));
+                    });
+
+
+
+                    $.ajaxSetup({
+                        headers: {
+                            'X-CSRF-TOKEN': $("meta[name='csrf-token']").attr('content')
+                        }
+                    });
+
+                    $.ajax({
+                        url: "{{ url('productions/save') }}",
+                        type: 'POST',
+                        data: {
+                            serial: seldata,
+                            supplierId: supplierId,
+                            prdCatId: prdCatId,
+                            prdName: prdName
+                        },
+                        success: function(e) {
+                            toastr.success(e + " Records added successfully");
+                            removeAll();
+                        },
+                        error: function() {
+                            toastr.error("Please check and fill all the fields");
+                        }
+
+                    });
+
+                });
             });
 
-            $('#del').attr('onclick', 'deleteField()');
-
-            // save form data
-            $('#saveData').click(function (e) {
-                e.preventDefault();
-
-                var supplierId = $("#supplier_id").val();
-                var prdCatId = $("#prd_cat").val();
-                var prdName = $("#production-name").val();
-                // serial numbers
-                var seldata = [];
-                $('.append').find(".item").each(function () {
-                    seldata.push($(this).attr("txt"));
-                });
-
-
-
-                $.ajaxSetup({
-                    headers: {
-                        'X-CSRF-TOKEN': $("meta[name='csrf-token']").attr('content')
-                    }
-                });
-
-                $.ajax({
-                    url: "{{url('productions/save')}}",
-                    type: 'POST',
-                    data:{
-                serial: seldata,
-                supplierId:supplierId,
-                prdCatId:prdCatId,
-                prdName:prdName
-                    },
-                    success:function(e){
-                        toastr.success(e + " Records added successfully");
-                        removeAll();
-                    },
-                    error:function(){
-                        toastr.error("Please check and fill all the fields");
-                    }
-                   
-                });
-
-        });
-        });
-
-    </script>
+        </script>
     @endpush
--- resources/views/layouts/backend/partials/topbar.blade.php
+++ resources/views/layouts/backend/partials/topbar.blade.php
@@ -1,51 +1,51 @@
   <!-- Navbar -->
   <nav class="main-header navbar navbar-expand navbar-white navbar-light">
-    <!-- Left navbar links -->
-    <ul class="navbar-nav">
-      <li class="nav-item">
-        <a class="nav-link" data-widget="pushmenu" href="#" role="button"><i class="fas fa-bars"></i></a>
-      </li>
-      <li class="nav-item d-none d-sm-inline-block">
-        <a href="index3.html" class="nav-link"></a>
-      </li>
-      <li>
-      <div class="col-sm-6 float-sm-right">
-           
-              
-             <!-- Authentication Links -->
-             @guest
-                            <li class="breadcrumb-item">
-                                <a class="nav-link" href="{{ route('login') }}">{{ __('Login') }}</a>
-                            </li>
-                            @if (Route::has('register'))
-                                <li class="nav-item">
-                                    <a class="nav-link" href="{{ route('register') }}">{{ __('Register') }}</a>
-                                </li>
-                            @endif
-                        @else
-                            <li class="nav-item dropdown">
-                                <a id="navbarDropdown" class="nav-link dropdown-toggle" href="#" role="button" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false" v-pre>
-                                    {{ Auth::user()->name }} <span class="caret"></span>
-                                </a>
+      <!-- Left navbar links -->
+      <ul class="navbar-nav">
+          <li class="nav-item  float-left">
+              <a class="nav-link" data-widget="pushmenu" href="#" role="button"><i class="fas fa-bars"></i></a>
+          </li>
+          <li class="nav-item d-none d-sm-inline-block">
+              <a href="index3.html" class="nav-link"></a>
+          </li>
+          <li>
+              <div class="col-sm-6 float-sm-right">
 
-                                <div class="dropdown-menu dropdown-menu-right" aria-labelledby="navbarDropdown">
-                                    <a class="dropdown-item" href="{{ route('logout') }}"
-                                       onclick="event.preventDefault();
-                                                     document.getElementById('logout-form').submit();">
-                                        {{ __('Logout') }}
-                                    </a>
 
-                                    <form id="logout-form" action="{{ route('logout') }}" method="POST" style="display: none;">
-                                        @csrf
-                                    </form>
-                                </div>
-                            </li>
-                        @endguest    
-            
+                  <!-- Authentication Links -->
+                  @guest
+              <li class="breadcrumb-item">
+                  <a class="nav-link" href="{{ route('login') }}">{{ __('Login') }}</a>
+              </li>
+              @if (Route::has('register'))
+                  <li class="nav-item">
+                      <a class="nav-link" href="{{ route('register') }}">{{ __('Register') }}</a>
+                  </li>
+              @endif
+          @else
+              <li class="nav-item dropdown">
+                  <a id="navbarDropdown" class="nav-link dropdown-toggle" href="#" role="button" data-toggle="dropdown"
+                      aria-haspopup="true" aria-expanded="false" v-pre>
+                      {{ Auth::user()->name }} <span class="caret"></span>
+                  </a>
+
+                  <div class="dropdown-menu dropdown-menu-right" aria-labelledby="navbarDropdown">
+                      <a class="dropdown-item" href="{{ route('logout') }}" onclick="event.preventDefault();
+                                                             document.getElementById('logout-form').submit();">
+                          {{ __('Logout') }}
+                      </a>
+
+                      <form id="logout-form" action="{{ route('logout') }}" method="POST" style="display: none;">
+                          @csrf
+                      </form>
+                  </div>
+              </li>
+          @endguest
+
           </div><!-- /.col -->
           </li>
-    </ul>
+      </ul>
 
-    
+
   </nav>
   <!-- /.navbar -->
